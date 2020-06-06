c0f54feb7fe118862098105d9c9099c000b4d93e
Alex Lucchesi <alexpdinfo@gmail.com>
2020-06-06 00:39:51 -0300

    Frontend React (typescript)

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
+import {Link} from 'react-router-dom';
 
 import './styles.css';
 
@@ -14,6 +16,11 @@ const Home = () => {
                 <main>
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+                    <Link to='/create_point'>
+                        <span><FiLogIn /></span>
+                        <strong>Cadastre um ponto de coleta</strong>
+                    </Link>
                 </main>
             </div>
         </div>
